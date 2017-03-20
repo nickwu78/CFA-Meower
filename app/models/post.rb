15400f9300bf68fb30714b3868b69a251e68c385
Nick Wu <nickwu78@gmail.com>
2017-03-20 16:10:53 +1100

    Integrate with AWS for image upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,15 @@ class Post < ApplicationRecord
   acts_as_likeable
   has_many :likes
   belongs_to :user
+  validate :picture_size
 
   mount_uploader :picture, PictureUploader
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
